3b63ab1312626ba82ccdfaa9cd53b5b024e91b95
v-t-9 <violeta.a.t.9@gmail.com>
2023-09-19 10:40:17 -0300

    Revert "ex94/ex95/ex96/ex97"
    This reverts commit e1917c6f910324fe7fe0c6057d4720bb4cbc6142.

--- ex96.py
+++
@@ -1,7 +0,0 @@
-import traceback
-# Write a Python program to print the current call stack
-
-
-if __name__ == "__main__":
-    print(traceback.print_stack())
-   
--- ex97.py
+++
@@ -1,5 +0,0 @@
-# Write a Python program to list the special variables used in the language.
-if __name__ == "__main___":
-    print(list(globals().keys()))
-
-

ac4ce59ae694cc57ca800339d137a00333fd1120
v-t-9 <violeta.a.t.9@gmail.com>
2023-09-19 10:42:31 -0300

    Revert "ex90/ex91/ex92/ex93"
    This reverts commit 3ac5009f32cc896dea5652e5ef4d471dad678b43.

--- ex90.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program to create a copy of its own source code.
-
-def copy_source():
-    with open(__file__) as f:
-        return f.read()
-    
-if __name__ == "__main__":
-    print(copy_source())
--- ex92.py
+++
@@ -1,1 +0,0 @@
-#  Write a Python program to define a string containing special characters in various forms.
